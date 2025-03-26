8be70e06cd754ae0a09c05d76c2302ca78e5ac67
AlexJumperman <alex.jumperman@gmail.com>
2025-03-26 18:57:55 +0200

    /users GET endpoint added

--- nodejs/udemy/task-manager/app.js
+++ nodejs/udemy/task-manager/app.js
@@ -7,13 +7,34 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/user', (req, res) => {
+app.post('/users', (req, res) => {
     const user = new User(req.body);
     user.save().then(r => {
-        res.send('success');
+        res.send(user);
     }).catch(e => {
-        res.send(e);
+        res.status(400).send(e);
     })
 })
+
+app.get('/users', (req, res) => {
+    User.find({}).then(r => {
+        res.send(r);
+    }).catch(e => {
+        res.status(500).send();
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+    User.findById(_id).then(r => {
+        if(!r){
+            return res.status(404).send();
+        }
+        res.send(r);
+    }).catch(e => {
+        res.status(500).send(e);
+    })
+})
+
 
 app.listen(port, () => {console.log('Server is up on port ' + port)})
