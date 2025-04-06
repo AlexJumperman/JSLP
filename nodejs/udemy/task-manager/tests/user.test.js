92f54f6f2f22b29e6b5ec4f9b0bf19eeac80fd29
AlexJumperman <alex.jumperman@gmail.com>
2025-04-06 11:59:39 +0300

    new user tests added

--- nodejs/udemy/task-manager/tests/user.test.js
+++ nodejs/udemy/task-manager/tests/user.test.js
@@ -1,11 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
+const userOneId = new mongoose.Types.ObjectId()
+const token = jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
 const userOne = {
+    _id: userOneId,
     name: "One",
     email: "test@test.com",
     password: "1q2w3e4r",
+    tokens: [{token}]
 }
 
 beforeEach(async () => {
@@ -22,10 +28,14 @@ test("Should signup new user", async () => {
 })
 
 test("Should login new user", async () => {
-    await request(app).post('/login').send({
+    const res = await request(app).post('/login').send({
         email: userOne.email,
         password: userOne.password,
     }).expect(200)
+
+    const user = await User.findById(userOneId)
+
+    expect(user.tokens.some(t => t.token === res.body.token)).toBe(true)
 })
 
 test("Fail to login", async () => {
@@ -33,4 +43,31 @@ test("Fail to login", async () => {
         email: userOne.email,
         password: "wrong-password",
     }).expect(401)
+})
+
+test("Get user profile", async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(200)
+})
+
+test("Not get user profile for anon user", async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test("Delete user profile", async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+
+    expect(user).toBeNull()
 })
